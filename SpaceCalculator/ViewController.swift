6a7fe5c1589fa409ca8bd2ecddee160d36507d04
firewood <karamaki@gmail.com>
2014-11-15 01:09:31 +0900

    initial commit

---
+++ SpaceCalculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SpaceCalculator
+//
+//  Created by Kenichi Aramaki on 2014/11/15.
+//  Copyright (c) 2014年 Pepsilover. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
